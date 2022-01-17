fc0f8e109804bb65e0e8afbe23a3eb0e68ac0ba4
杨嘉祺 <duckmoutain@yangjiaqideMBP.lan>
2022-01-17 08:10:53 +0800

    feat: init Commit

---
+++ GameOnews/Shared/GameOnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOnewsApp.swift
+//  Shared
+//
+//  Created by 杨嘉祺 on 2022/1/17.
+//
+
+import SwiftUI
+
+@main
+struct GameOnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
